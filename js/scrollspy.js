08d5c43b1bf4c07749b781d94edb6c1f20c1e30d
MathematicsCLtd <jesse@mathematicsconsulting.com>
2018-10-17 15:16:25 -0400

    Add scrollspy JavaScript file

---
+++ js/scrollspy.js
@@ -0,0 +1,13 @@
+$(function() {
+    $(window).on('activate.bs.scrollspy'), function() {
+        var hash = $('.site-nav')
+            .find('a.active')
+            .attr('href');
+
+        if (hash !== '#page-hero') {
+            $('header nav').addClass('inbody');
+        } else {
+            $('header nav').removeClass('inbody');
+        }
+    }
+});
