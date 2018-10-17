0589b497f6bc6f97f70f0945f1a9078a4fea50a5
MathematicsCLtd <jesse@mathematicsconsulting.com>
2018-10-17 15:25:31 -0400

    Open window function bracket

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,13 +1,14 @@
 $(function() {
-    $(window).on('activate.bs.scrollspy'), function() {
-        var hash = $('.site-nav')
-            .find('a.active')
-            .attr('href');
+    $(window).on('activate.bs.scrollspy',
+        function() {
+            var hash = $('.site-nav')
+                .find('a.active')
+                .attr('href');
 
-        if (hash !== '#page-hero') {
-            $('header nav').addClass('inbody');
-        } else {
-            $('header nav').removeClass('inbody');
-        }
-    }
-});
+            if (hash !== '#page-hero') {
+                $('header nav').addClass('inbody');
+            } else {
+                $('header nav').removeClass('inbody');
+            }
+        });
+    });
